71e643fbc06283f97e15111993f38f926743ec98
Fiwal <fiwalczowski@gmail.com>
2022-12-13 19:04:00 +0100

    aktualna wersja

--- projekt.py
+++ projekt.py
@@ -4,11 +4,17 @@ window = Tk()
 
 window.minsize(640, 480)
 window.maxsize(640, 480)
+photo = PhotoImage(file="Zrzut ekranu 2022-12-11 193101.png")
 
-label1 = Frame(window, width=640, height=400, bd=3, bg="black")
-label2 = Label(window, text="tekst", font=("Arial", 24), bd=3, bg="green")
+label1 = Label(window,
+               text="tekst",
+               font=("Arial", 24),
+               bd=3,
+               bg="black",
+               fg="white",
+               image=photo,
+               compound="top")
 
 label1.pack()
-label2.pack()
 
 window.mainloop()
