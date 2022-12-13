ce6ef5fdc390365b8387400c6145d86dd8c42f2e
Fiwal <fiwalczowski@gmail.com>
2022-12-13 18:50:19 +0100

    aktualna wersja

--- projekt.py
+++ projekt.py
@@ -5,4 +5,10 @@ window = Tk()
 window.minsize(640, 480)
 window.maxsize(640, 480)
 
+label1 = Frame(window, width=640, height=400, bd=3, bg="black")
+label2 = Label(window, text="tekst", font=("Arial", 24), bd=3, bg="green")
+
+label1.pack()
+label2.pack()
+
 window.mainloop()
